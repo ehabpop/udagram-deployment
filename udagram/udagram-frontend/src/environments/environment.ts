b851f7384d8a635b106d9099fa1ab7849c43ba95
ehabpop <ehabpop@gmail.com>
2022-07-22 18:01:36 +0200

    update deploy.sh file for frontend

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -10,6 +10,7 @@ export const environment = {
 };
 
 /*
+ *  http://udagram2022.s3-website-us-east-1.amazonaws.com/
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
  *
